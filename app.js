7762b37572e76018e9fb0f88803d9311b73cd7f7
Tae Hoon Kim <taehoonkim@taes-mbp.mynetworksettings.com>
2022-12-28 22:00:33 -0500

    idk

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var redirect_uri = "https://google.com/"; // change this your value
+var redirect_uri = "https://thkimsea43.github.io/"; // change this your value
 //var redirect_uri = "http://127.0.0.1:5500/index.html";
 
 
